7a1abd78bd2ffaaa2defb6ce8d78432ac671deb2
MarkZhang <61927849+CN-Mark-Zhang@users.noreply.github.com>
2020-11-13 15:26:25 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', 'Mark Zhang-Video');//网站名字
+define('TITLENAME', '搞事情');//网站名字
+define('EN2DEKEY', '  DJhEdFje93Jjer2jd0FSDUjdjNweh4fkwmr8246Rb');//一个随机字符串，加解密KEY
 define('EMAIL', '123@123.com');//邮箱
 ?>
