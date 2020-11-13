4490ff2a2ae1e3cb17411d53167270e8178b0f0a
MarkZhang <61927849+CN-Mark-Zhang@users.noreply.github.com>
2020-11-13 15:31:55 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -5,5 +5,5 @@ define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Mark Zhang-Video');//网站名字
 define('TITLENAME', '搞事情');//网站名字
 define('EN2DEKEY', '  DJhEdFje93Jjer2jd0FSDUjdjNweh4fkwmr8246Rb');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'markzhang-zx@yandex.com');//邮箱
 ?>
